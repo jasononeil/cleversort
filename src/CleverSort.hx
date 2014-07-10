f985f1b383e07d5aa59460b88ab3f5aa89753a94
Jason O'Neil <jason.oneil@gmail.com>
2014-07-10 17:02:36 +0800

    Added documentation, catch errors

--- src/CleverSort.hx
+++ src/CleverSort.hx
@@ -1,8 +1,86 @@
-import haxe.macro.Context;
-import haxe.macro.Expr;
-using tink.macro.tools.MacroTools;
+package cleversort;
 
-/** Syntax sugar for creating arrays that sorts on multiple properties */
+#if macro
+	import haxe.macro.Context;
+	import haxe.macro.Expr;
+	using tink.MacroApi;
+#end
+
+/** Syntax sugar for creating arrays that sorts on multiple properties
+
+```
+// Imagine we have this data:
+
+class Person {
+	var firstName:String;
+	var surname:String;
+	var age:Int;
+	var postcode:PostCode;
+}
+class PostCode {
+	var state:StateEnum;
+	var number:Int;
+}
+var array:Array<Person> = ...;
+
+// We call "using CleverSort" so that we can use the `cleverSort()` function on any array
+
+using CleverSort;
+
+//
+// Example 1:
+// Sort by surname, firstName, age
+//
+
+array.cleverSort(
+	_.firstName.toLowerCase()
+	_.surname.toLowerCase(),
+	_.age
+);
+
+// Which is the equivalent of writing
+
+array.sort( function(i1,i2) {
+	var cmp:Int;
+	cmp = Reflect.compare(i1.firstName.toLowerCase(),i2.firstName.toLowerCase());
+		if ( cmp!=0 ) return cmp;
+	cmp = Reflect.compare(i1.surname.toLowerCase(),i2.surname.toLowerCase());
+		if ( cmp!=0 ) return cmp;
+	cmp = Reflect.compare(i1.age,i2.age);
+		if ( cmp!=0 ) return cmp;
+	return 0;
+});
+
+//
+// Example 2:
+// Sort by state, postcode, surname, firstname
+//
+
+array.cleverSort(
+	Type.enumConstructor( _.postcode.state ),
+	_.postcode.number,
+	_.surname,
+	_.firstName
+);
+
+// Which is the equivalent of writing
+
+array.sort( function(i1,i2) {
+	var cmp:Int;
+	cmp = Reflect.compare(Type.enumConstructor(i1.postcode.state),Type.enumConstructor(i2.postcode.state));
+		if ( cmp!=0 ) return cmp;
+	cmp = Reflect.compare(i1.postcode.number,i2.postcode.number);
+		if ( cmp!=0 ) return cmp;
+	cmp = Reflect.compare(i1.surname,i2.surname);
+		if ( cmp!=0 ) return cmp;
+	cmp = Reflect.compare(i1.firstName,i2.firstName);
+		if ( cmp!=0 ) return cmp;
+	return 0;
+});
+```
+
+As you can see, the syntax is much more readable.  Winning!
+*/
 class CleverSort
 {
 	macro public static function cleverSort(arr:ExprOf<Array<Dynamic>>, filters:Array<Expr>)
@@ -32,6 +110,6 @@ class CleverSort
 		statements.push( macro return 0 );
 
 		// Return it as an expression: arr.sort(sortFn);
-		return macro $arr.sort($sortFn);
+		return macro try $arr.sort($sortFn) catch( e:Dynamic ) { trace('Cleversort failed: $e'); };
 	}
 }
